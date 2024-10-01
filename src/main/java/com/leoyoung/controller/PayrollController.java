b959a7bf939d4ec7e56642ba2038d77f8e237399
Leoyoung <leoyoung8372@gmail.com>
2024-10-01 09:52:13 +0800

    增加判断逻辑：当生成PDF文件时，不存在输入的薪资记录，会显示提示

--- src/main/java/com/leoyoung/controller/PayrollController.java
+++ src/main/java/com/leoyoung/controller/PayrollController.java
@@ -15,18 +15,25 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/payroll")
 public class PayrollController {
+
     @Autowired
     private PayrollService payrollService;
 
     @Autowired
     private PdfGenerator pdfGenerator;
 
-    //生成PDF文件
+    // 生成PDF文件
     @GetMapping("/pdf")
-    public ResponseEntity<byte[]> getPayrollPdf(@RequestParam("employeeId") String employeeId, @RequestParam("salaryDate") String salaryDate) {
+    public ResponseEntity<?> getPayrollPdf(@RequestParam("employeeId") String employeeId,
+                                           @RequestParam("salaryDate") String salaryDate) {
 
         // 根据员工ID和工资日期获取工资单信息
         Payroll payroll = payrollService.getPayrollByEmployeeIdAndDate(employeeId, salaryDate);
+
+        if (payroll == null) {
+            // 如果工资记录不存在，返回404 Not Found和错误信息
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("不存在该薪资记录。");
+        }
 
         // 创建一个字节数组输出流用于存储PDF文件
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
@@ -34,27 +41,29 @@ public class PayrollController {
             // 使用PDF生成器生成PDF文件并写入输出流
             pdfGenerator.generatePdf(payroll, outputStream);
         } catch (IOException | java.io.IOException e) {
-            // 如果发生异常,打印堆栈信息并返回内部服务器错误响应
+            // 如果发生异常，打印堆栈信息并返回内部服务器错误响应
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
 
         // 从输出流中获取PDF文件的字节数组
-        byte[] pdfBytes = outputStream.toByteArray();
+        byte[] pdfBytes = outputStream.toByteArray ();
 
         // 根据员工ID和工资日期动态生成PDF文件名
         String fileName = "payroll_" + employeeId + "_" + salaryDate + ".pdf";
 
-        // 设置HTTP响应头,指定内容类型为PDF,并设置文件名
+        // 设置HTTP响应头，指定内容类型为PDF，并设置文件名
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_PDF);
         headers.setContentDispositionFormData("attachment", fileName);
-        System.out.printf(fileName);
+
+        // 暴露 Content-Disposition 响应头
+        headers.add("Access-Control-Expose-Headers", "Content-Disposition");
+        System.out.println(fileName);
 
         // 返回包含PDF文件字节数组的HTTP响应
         return ResponseEntity.ok()
                 .headers(headers)
                 .body(pdfBytes);
     }
-
 }
