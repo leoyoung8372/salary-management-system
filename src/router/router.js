572a1f5d02c9382693031e23104c2544b101074e
Leoyoung <leoyoung8372@gmail.com>
2024-10-26 14:14:37 +0800

    END

--- src/router/router.js
+++ src/router/router.js
@@ -15,7 +15,7 @@ const router = createRouter({
     routes: [
         {  
             path: '/',  
-            redirect: '/CheckIn' // 添加默认路由 (默认显示) 
+            redirect: '/CheckIn' // 添加默认路由 (默认显示的页面) 
         }, 
 
         {
